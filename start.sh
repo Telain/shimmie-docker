f950a851fe34f83735fe4e686217ed10dc17150b
Telain <telain000@gmail.com>
2016-07-28 04:23:12 -0500

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOCKFILE=/run/apache2/apache2.pid
+
+# Previous apache should execute successfully:
+
+[ -f $LOCKFILE ] && exit 0
+
+# Upon exit, remove lockfile.
+
+trap "{ rm -f $LOCKFILE ; exit 255; }" EXIT SIGTERM
+
+/usr/sbin/apache2 -D FOREGROUND
+
+exit 0
